4978209e983000f534b4a5980ff76f815d4f1c04
mfotgs <michael.f@onthegosystems.com>
2025-09-04 10:20:36 +0200

    PTC test-cbatticon-localize (1947): Automatic Translations

---
+++ cbatticon-fr.l10n.php
@@ -0,0 +1,38 @@
+<?php
+return ['domain'=>NULL,'plural-forms'=>'nplurals=INTEGER; plural=EXPRESSION;','language'=>'fr','project-id-version'=>'cbatticon 1.6.13','pot-creation-date'=>'2022-04-27 21:27+0200','po-revision-date'=>'YEAR-MO-DA HO:MI+ZONE','messages'=>['Display the big version'=>'Afficher la grande version','Display debug information'=>'Afficher les informations de débogage','Set update interval (in seconds)'=>'Définir l\'intervalle de mise à jour (en secondes)','Set icon type (\'standard\', \'notification\' or \'symbolic\')'=>'Définir le type d\'icône (\'standard\', \'notification\' ou \'symbolic\')','Set low battery level (in percent)'=>'Définir le niveau de batterie faible (en pourcentage)','Set critical battery level (in percent)'=>'Définir le niveau de batterie critique (en pourcentage)','Command to execute when low battery level is reached'=>'Commande à exécuter lorsque le niveau de batterie faible est atteint','Command to execute when critical battery level is reached'=>'Commande à exécuter lorsque le niveau de batterie critique est atteint','Command to execute when left clicking on tray icon'=>'Commande à exécuter lors d\'un clic gauche sur l\'icône dans la barre système','Hide the notification popups'=>'Masquer les notifications contextuelles','List available icon types'=>'Lister les types d\'icônes disponibles','List available power supplies (battery and AC)'=>'Lister les sources d\'alimentation disponibles (batterie et secteur)','[BATTERY ID]'=>'[IDENTIFIANT DE BATTERIE]','Cannot parse command line arguments: %s
+'=>'Impossible d\'analyser les arguments de ligne de commande : %s
+','cbatticon: a lightweight and fast battery icon that sits in your system tray
+'=>'cbatticon : une icône de batterie légère et rapide qui réside dans votre barre système
+','version %s
+'=>'version %s
+','List of available power supplies:
+'=>'Liste des sources d\'alimentation disponibles :
+','List of available icon types:
+'=>'Liste des types d\'icônes disponibles :
+','available'=>'disponible','unavailable'=>'indisponible','Unknown icon type: %s
+'=>'Type d\'icône inconnu : %s
+','No icon type found!
+'=>'Aucun type d\'icône trouvé!
+','Invalid update interval! It has been reset to default (%d seconds)
+'=>'Intervalle de mise à jour invalide ! Il a été réinitialisé à la valeur par défaut (%d secondes)
+','Invalid low level! It has been reset to default (%d percent)
+'=>'Niveau faible invalide ! Il a été réinitialisé à la valeur par défaut (%d pour cent)
+','Invalid critical level! It has been reset to default (%d percent)
+'=>'Niveau critique invalide ! Il a été réinitialisé à la valeur par défaut (%d pour cent)
+','Critical level is higher than low level! They have been reset to default
+'=>'Le niveau critique est supérieur au niveau faible ! Ils ont été réinitialisés aux valeurs par défaut
+','type: %-*.*s	id: %-*.*s	path: %s
+'=>'type : %%-*.*s	identifiant : %%-*.*s	chemin : %s
+','Battery'=>'Batterie','AC'=>'Secteur','Cannot open sysfs directory: %s (%s)
+'=>'Impossible d\'ouvrir le répertoire sysfs : %s (%s)
+','No battery with suffix %s found!
+'=>'Aucune batterie avec le suffixe %s trouvée !
+','No battery nor AC power supply found!
+'=>'Aucune batterie ni alimentation secteur trouvée !
+','AC only, no battery!'=>'Alimentation secteur uniquement, pas de batterie !','Spawning low battery level command in 5 seconds: %s'=>'Exécution de la commande de niveau de batterie faible dans 5 secondes : %s','Skipping low battery level command, no longer discharging'=>'Annulation de la commande de niveau de batterie faible, la décharge n\'est plus en cours','Cannot spawn low battery level command: %s
+'=>'Impossible d\'exécuter la commande de niveau de batterie faible : %s
+','Cannot spawn low battery level command!'=>'Impossible d\'exécuter la commande de niveau de batterie faible !','Spawning critical battery level command in 30 seconds: %s'=>'Exécution de la commande de niveau de batterie critique dans 30 secondes : %s','Skipping critical battery level command, no longer discharging'=>'Annulation de la commande de niveau de batterie critique, la décharge n\'est plus en cours','Cannot spawn critical battery level command: %s
+'=>'Impossible d\'exécuter la commande de niveau de batterie critique : %s
+','Cannot spawn critical battery level command!'=>'Impossible d\'exécuter la commande de niveau de batterie critique !','Cannot spawn left click command: %s
+'=>'Impossible d\'exécuter la commande de clic gauche : %s
+','Cannot spawn left click command!'=>'Impossible d\'exécuter la commande de clic gauche !','Battery is missing!'=>'La batterie est manquante !','Battery status is unknown!'=>'L\'état de la batterie est inconnu !','Battery is charged!'=>'La batterie est chargée !','Battery is discharging (%i%% remaining)'=>'La batterie se décharge (%i%% restant)','Battery is not charging (%i%% remaining)'=>'La batterie ne se charge pas (%i%% restant)','Battery level is low! (%i%% remaining)'=>'Le niveau de batterie est faible ! (%i%% restant)','Battery level is critical! (%i%% remaining)'=>'Le niveau de batterie est critique ! (%i%% restant)','Battery is charging (%i%%)'=>'La batterie est en charge (%i%%)','%d minute'=>'%d minute' . "\0" . '%d minutes' . "\0" . '%d minutes','%d hour, %s remaining'=>'%d heure, %s restant' . "\0" . '%d heures, %s restant' . "\0" . '%d heures, %s restant','%d minute remaining'=>'%d minute restante' . "\0" . '%d minutes restantes' . "\0" . '%d minutes restantes']];
